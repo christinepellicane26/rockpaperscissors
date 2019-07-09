a36f981ba68dc8d1fde6c3dcdb62401cdd535ee2
Student <jsitbon@qcc.cuny.edu>
2019-07-09 17:49:45 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,56 @@
 import React, {Component} from 'react';
+import PlayerCard from './PlayerCard';
 
 class Game extends Component {
-  render() {
+  constructor(){
+    super()
+    this.signs = ["rock", "scissors", "paper"]
+    this.state = {
+      playerOne: "rock",
+      playerTwo:  "scissors"
+      }
+    }
+
+    playGame =()=> {
+      //methods go here, adding functionality
+      this.setState({
+        playerOne: this.signs[Math.floor(Math.random()*3)],
+        playerTwo: this.signs[Math.floor(Math.random()*3)],
+      
+      })
+      
+      }
+
+      decideWinner =()=> {
+        const playerOne = this.state.playerOne
+        const playerTwo = this.state.playerTwo
+        if (playerOne === playerTwo) {
+          return "It's a Tie!"
+        }
+        
+         else if ((playerOne === "rock" && playerTwo ==="scissors")||
+         (playerOne === "paper" && playerTwo ==="rock")) {
+         
+         return "Player Two Wins!"
+         } else if (playerOne ==="scissors" && playerTwo ==="paper"){
+         
+         return "Player Two Wins!"
+      }
+    }
+
+  render(){
     return (
       <div className="style">
-      <div>Put the playerCard components here</div>
-      <div className="winner" Place the winner here </div>
-      <button type="button" Play the Game </button>
+      <div>
+    <PlayerCard sign={this.state.playerOne} /> 
+    <PlayerCard sign={this.state.playerTwo} />
+    </div>
+      <div className="winner"> {this.decideWinner()} </div>
+      <button type="button" onClick={this.playGame}> Play the Game! </button>
      </div>
     )
   }
-}
+  }
 
 
-
-export default App;
+export default Game;
