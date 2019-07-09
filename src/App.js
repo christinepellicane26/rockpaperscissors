152d8f0dfb8718086a2c192fc626e4396f2ecd4d
Student <jsitbon@qcc.cuny.edu>
2019-07-09 16:00:48 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Game extends Component {
+  render() {
+    return (
+      <div className="style">
+      <div>Put the playerCard components here</div>
+      <div className="winner" Place the winner here </div>
+      <button type="button" Play the Game </button>
+     </div>
+    )
+  }
 }
+
+
 
 export default App;
